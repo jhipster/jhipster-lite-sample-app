03668a4e9a2f6900055653aa36ff60f15cddda4a
Pascal Grimaud <pascalgrimaud@gmail.com>
2024-02-09 14:02:27 +0100

    Release v1.4.0

--- src/main/java/tech/jhipster/lite/sample/wire/frontend/infrastructure/primary/RedirectionResource.java
+++ src/main/java/tech/jhipster/lite/sample/wire/frontend/infrastructure/primary/RedirectionResource.java
@@ -2,6 +2,7 @@ package tech.jhipster.lite.sample.wire.frontend.infrastructure.primary;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import tech.jhipster.lite.sample.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
 
 @Controller
@@ -21,7 +22,7 @@ class RedirectionResource {
       "/*/*" + TRAILING_DIR_PATTERN,
     }
   )
-  public String redirectApi() {
+  public String redirectApi(@PathVariable(required = false) String file, @PathVariable(required = false) String trailingDir) {
     return "forward:/";
   }
 }
